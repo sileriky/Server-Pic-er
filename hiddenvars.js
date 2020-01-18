ffbfe0f8a626d04ce5aef58b0675e25f24ce12b2
Eric Vasile <32675397+sileriky@users.noreply.github.com>
2020-01-18 02:30:56 +0000

    Create hiddenvars.js

---
+++ hiddenvars.js
@@ -0,0 +1,5 @@
+//This file contains values unique to the bot, thus they should be replaced with the appropriate values, otherwise node will spit out errors.
+
+exports.token = 'Discord bot token';
+exports.CSEID = 'Google search engine ID';
+exports.APIKEY = 'Google API key';
